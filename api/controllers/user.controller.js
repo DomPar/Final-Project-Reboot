45fbbdfba332628b105cb84c8358cd70b997678c
reboot <djps94@outlook.com>
2024-02-26 14:45:03 +0000

    UpdateUser, UpdatePassword, DeleteUser ready

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,8 +53,83 @@ const getOneUser = async (req, res) => {
     }
 }
 
+const updateUser = async (req, res) => {
+    try {
+        const [result] = await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!result) {
+            return res.status(404).send('User not found')
+        } else {
+        res.status(200).json({
+            message: 'User updated succesfully',
+            result: req.body
+        }) 
+    }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating user',
+            result: error 
+            })
+    }
+}
+
+
+const updatePassword = async (req, res) => {
+    try {
+        const [result] = await User.update(req.body, {
+            where: {
+                id: req.params.id
+            }, 
+            fields: ["password"]
+        })
+        if(!result) {
+            return res.status(404).send('Password not found')
+        } else {
+        res.status(200).json({
+            message: 'Password updated succesfully',
+            result: req.body
+        }) 
+    }
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating Password',
+            result: error 
+            })
+    }
+}
+
+const deleteUser = async (req, res) => {
+    try {
+        const user = await User.destroy({
+            where: {
+                id: req.params.id
+            }
+        })
+        if(!user) {
+            return res.status(404).send('User not found')
+        } else {
+        res.status(200).json({
+            message: 'User deleted succesfully',
+            result: user
+        }) 
+    }
+
+    } catch (error) {
+        res.status(500).json({
+            message: 'Error updating User',
+            result: error 
+            })
+    }
+}
+
 module.exports = {
     createUser,
     getAllUsers,
-    getOneUser
+    getOneUser,
+    updateUser,
+    updatePassword,
+    deleteUser
 }
--- api/routers/user.router.js
+++ api/routers/user.router.js
@@ -3,7 +3,10 @@ const router = require('express').Router()
 const { 
     createUser,
     getAllUsers,
-    getOneUser
+    getOneUser,
+    updateUser,
+    updatePassword,
+    deleteUser
 } = require ('../controllers/user.controller')
 
 router.post('/', createUser)
@@ -11,6 +14,12 @@ router.post('/', createUser)
 router.get('/', getAllUsers)
 
 router.get('/:id', getOneUser)
+
+router.put('/:id', updateUser)
+
+router.patch('/:id', updatePassword)
+
+router.delete('/:id', deleteUser)
 
 
 module.exports = router
