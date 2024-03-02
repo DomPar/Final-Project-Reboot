47c5064a566d60dab01ac585b78865353e61bbaa
Domingo Pardo Santana <djps94@outlook.com>
2024-03-02 17:35:28 +0000

    aea

---
+++ frontend/FinalProjectReact/src/componentes/ShelterList/ShelterItem.jsx
@@ -0,0 +1,16 @@
+import './ShelterList.css'
+
+const ShelterItem = () => {
+  return (
+    <div id="shelter">
+      <div id="shelter-profile" style={{backgroundImage: `url('https://savehomelessanimals.org/wp-content/uploads/Home-Adopt-a-Dog-image-1024x1024.jpg.webp')`}}>
+
+      </div>
+      <div id="shelter-name">
+
+      </div>
+    </div>
+  )
+}
+
+export default ShelterItem
